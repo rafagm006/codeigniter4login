97c845cd90e5fb32242f77f703a76f79881450bc
ragranados <ragranados@uveg.edu.mx>
2020-12-01 22:45:12 -0600

    Panel principal.

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,6 +2,27 @@
   <div class="row">
     <div class="col-12">
       <h1>Hello, <?= session()->get('firstname') ?></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aut itaque minima optio, quas repellat sequi veniam. Accusamus animi autem blanditiis cum dicta enim hic, iusto nulla optio similique, tempora?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi deserunt est ex libero perspiciatis, provident sint suscipit ut. Accusantium amet consectetur deleniti dicta error nemo non officia saepe, ullam vitae.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid culpa cum debitis dignissimos dolor expedita facilis ipsum, labore obcaecati pariatur perferendis quis ratione repudiandae sed sunt temporibus totam ullam.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, adipisci at consequuntur dicta dolor eum ex fugit illum laudantium molestiae nemo nobis, odio perspiciatis quasi sed tempora, veniam? Architecto, maxime!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab adipisci alias aliquam autem beatae commodi doloribus ex, excepturi explicabo fuga ipsa praesentium qui recusandae, rem temporibus vel voluptas voluptate.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores at commodi corporis dignissimos dolore est excepturi explicabo, fugit iure maxime molestiae numquam, odio, porro provident recusandae rem ut. Ut!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur culpa deserunt expedita id inventore officia porro, reprehenderit saepe vitae voluptatibus? Consectetur cumque distinctio harum neque nostrum quis tempora unde voluptates.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto, atque distinctio odio pariatur reiciendis sunt? A est in qui totam! Harum natus optio recusandae rem voluptate. Atque, obcaecati, praesentium.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eius exercitationem iste omnis quasi quisquam soluta? Alias aut expedita facilis inventore maxime molestiae officiis pariatur quae tempore vitae! Animi, sed.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias aliquid architecto dolores ipsa laudantium modi, molestiae necessitatibus optio praesentium sint sunt tempora, voluptatem. Dolorem mollitia necessitatibus porro quod ullam?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi aspernatur commodi cum debitis, eligendi enim eos illum in incidunt laborum modi nam necessitatibus optio pariatur praesentium saepe. Aliquid, eius.</p>
     </div>
   </div>
 </div>
